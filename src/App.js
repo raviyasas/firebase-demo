e42911e2be9d1c38239f379e826202942685d99f
Ravi Yasas <raviyasas@live.com>
2022-07-17 01:54:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { AddTask } from './components/AddTask';
+import { TaskList } from './components/TaskList';
+
+function App() {
+  return (
+    <div className="App">
+      <AddTask/>
+      <TaskList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  // Please add your configurations here
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+export {db}
